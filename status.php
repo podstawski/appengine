72cc4125507cd3aa965387e3072b663593ecd919
Piotr Podstawski <piotr@webkameleon.com>
2016-01-26 09:20:47 +0000

    cron

--- status.php
+++ status.php
@@ -3,14 +3,10 @@
     
     $status=getStatus($service);
     if ($status!='RUNNING') return;
-    
-    
+        
     $config = include __DIR__.'/ini.php';
     
     $canStop=file_get_contents($config['status']);
     
+    if ($canStop=='1') stopInstance($service);
     
-
-    //if ($status=='TERMINATED') startInstance($service);
-    
-    die($status);
